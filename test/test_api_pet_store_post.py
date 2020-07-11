b2e08a819ea7e4b10dcece403d1c6b24dd05f8c1
Gabriela Vilaro <gabriela.vilaro@uala.com.ar>
2020-07-11 16:04:55 -0300

    add test post pet store

--- test/test_api_pet_store_post.py
+++ test/test_api_pet_store_post.py
@@ -3,45 +3,45 @@ import json
 import unittest
 
 
-class TestApiPetStorePut(unittest.TestCase):
+class TestApiPost(unittest.TestCase):
 
     def setUp(self):
         self.url = f"https://petstore.swagger.io/v2/pet"
         self.headers = {"Content-Type": "application/json", "accept": "application/json"}
         self.body = """{
-          "id": 10,
+          "id": 501,
           "category": {
             "id": 3,
-            "name": "Conejo Malo"
+            "name": "Perros"
           },
-          "name": "Bod Conejito",
+          "name": "Luna",
           "photoUrls": [
-            "https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg",
+            "url1",
             "url2"
           ],
           "tags": [
             {
               "id": 1,
-              "name": "tag3"
+              "name": "Rita"
             },
             {
               "id": 2,
-              "name": "tag4"
+              "name": "Lola"
             }
           ],
           "status": "available"
         }"""
 
-    def test_api_pet_store_put(self):
-        """Este test hace un put a la url, cmabiando el nombre de la mascota"""
+    def test_post_api_pet_store(self):
+        """Este test envía un post a la url del pet store y verifica sus datos"""
 
-        response = requests.put(self.url, headers=self.headers, data=self.body)
+        response = requests.post(self.url, headers=self.headers, data=self.body)
         json_response = json.loads(response.text)
         print(json.dumps(json_response, indent=3))
 
-        assert json_response['id'] is not None, "Esta Vacío"
-        assert json_response['category']['name'] == "Conejo Malo", 'No coincide'
-        assert json_response['category']['id'] == 3, 'No coincide'
+        assert json_response['id'] == 501, 'No coincide'
+        assert json_response['category']['name'] == 'Perros', 'No coincide'
+        assert json_response['tags'][0]['name'] == 'Rita', 'No coincide'
 
     def tearDown(self):
         pass
