c6cb67f1981c37ddc7ce4cc462135d92da0750f1
Gabriela Vilaro <gabriela.vilaro@uala.com.ar>
2020-07-26 12:04:26 -0300

    test unification of the same apis

--- tests/test_api_twitter.py
+++ tests/test_api_twitter.py
@@ -15,12 +15,25 @@ class TestApiTwitter(unittest.TestCase):
         self.apiBaseTwitter = BaseApiTwitter()
 
     def test_api_twitter_with_tweet(self):
+        '''Este test hace un get sobre los datos de un tweet y los valida'''
+
         tweet_response = requests.get(self.apiBaseTwitter.tweet, headers=self.apiBaseTwitter.header)
         json_response = json.loads(tweet_response.text)
         print(json.dumps(json_response, indent=3))
         assert json_response['statuses'][0]['id_str'] == '1286000604771487746'
         assert json_response['statuses'][0]['id'] == 1286000604771487746
         assert json_response['statuses'][0]['truncated'] == True
+
+    def test_get_response_with_token(self):
+        '''Este test primero genera el token necesario para acceder a la cuenta y traer los datos e imprime el tokwn_type
+            y el access token, luego trae los datos del usaurio y hace las validaciones la API se debe solicitar a
+          Twitter en https://developer.twitter.com/'''
+
+        print(json.dumps(self.apiBaseTwitter.Authorization_response, indent=3))
+        tweet_response = requests.get(self.apiBaseTwitter.tweet, headers=self.apiBaseTwitter.header)
+        response = json.loads(tweet_response.text)
+        print(json.dumps(response, indent=3))
+        assert response['statuses'][0]['id_str'] == '1286000604771487746', 'Not match'
 
     def tearDown(self):
         pass
